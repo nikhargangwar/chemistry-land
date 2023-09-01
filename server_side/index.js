80d94826d7109296448f0f24876f5a23106029c0
Nikhar Gangwar <62029765+nikhargangwar@users.noreply.github.com>
2023-09-01 21:14:03 +0530

    add: server side basic code(express + sequelize code setup)

---
+++ server_side/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+// const router = require('./routes')
+// const { PORT } = require('./config')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+// app.use('/api', router)
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.json({message:'hello from api'})
+})
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log(`Server running successfullyat port ${PORT}`)
+  } else {
+    console.log('Error occurred! Server failed to run')
+  }
+})
