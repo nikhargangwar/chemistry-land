c85378489d45485deadbedb701d9cb9e71177aa2
Nikhar Gangwar <62029765+nikhargangwar@users.noreply.github.com>
2023-09-03 11:21:51 +0530

    Add: home page

--- client_side/src/app/app-routing.module.ts
+++ client_side/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardListComponent } from './components/card-list/card-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CardListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client_side/src/app/app.module.ts
+++ client_side/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
